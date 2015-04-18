23c2ddb5b4dfb3b284a2b401ffe5a6c9c8b7769b
critiqjo <john.ch.fr@gmail.com>
2015-04-18 13:23:29 +0530

    update env.zsh

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,9 +1,12 @@
-alias ls='ls -GFh --color'
-alias ll='ls -lA'
+alias ls='ls --color'
+alias ll='ls -lAh'
+alias cp="cp --reflink=auto"
+[ -z "$NVIM_LISTEN_ADDRESS" ] || alias :="${HOME}/.local/bin/nvimex.py"
 
 #export TERM=xterm-256color
 export EDITOR='nvim'
 export HISTSIZE=1024
 export SAVEHIST=2048
 export HISTFILE=~/.cache/zsh/history
+export PATH=$PATH:${HOME}/.local/bin
 export WORDCHARS='*?_-.[]~=&;!#$%^(){}<>' # doesn't include '/'
