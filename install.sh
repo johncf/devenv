e1f471293d5ca05aed67ec0159a127004dfdcdbb
critiqjo <john.ch.fr@gmail.com>
2015-05-30 22:56:03 +0530

    +install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+SCR_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $HOME
+ln -s $SCR_DIR/zshrc .zshrc
+ln -s $SCR_DIR/zsh .zsh
+ln -s $SCR_DIR/Xdefaults .Xdefaults
+mkdir -p .config .nvim .cache/zsh
+cd .config
+ln -s $SCR_DIR/config/Xdefaults.d ./
+cp $SCR_DIR/config/mimeapps.list ./
+cd ../.nvim
+ln -s $SCR_DIR/nvim/colors ./
+ln -s $SCR_DIR/nvim/plugin ./
+ln -s $SCR_DIR/nvim/nvimrc ./
+mkdir swps undos
+
+head -5 $SCR_DIR/nvim/nvimrc | tail -3 | cut -c 5- | bash
+head -11 $SCR_DIR/config/Xdefaults.d/urxvt | tail -8 | cut -c 3- | bash
