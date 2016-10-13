3355ee721026feff0360a2eadc1bf58b23d01793
critiqjo <john.ch.fr@gmail.com>
2016-10-13 13:29:04 +0530

    updates

--- local/bin/date-sync.sh
+++ local/bin/date-sync.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo date -s "$(wget -O /dev/null -S "http://www.google.com/" 2>&1 | sed -n 's/^[ ]*[dD]ate:[ ]*//p' | head -1l | awk '{print $1, $3, $2, $5, "GMT", $4 }' | sed 's/,//')"
+
+sudo date -s "$(curl -v "http://www.google.com/" 2>&1 >/dev/null | sed -n 's/^<[ ][dD]ate:[ ]*//p' | head -1l | awk '{print $1, $3, $2, $5, "GMT", $4 }' | sed 's/,//')"
+
+# sudo timedatectl set-local-rtc 0
